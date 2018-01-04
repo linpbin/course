0648cc3e4d1c9d4281ef84510d2d6d3a45c52c68
linpbin <linpbin@qq.com>
2018-01-05 00:00:32 +0800

    1
    1

--- src/main/java/com/my/course/login/web/LoginController.java
+++ src/main/java/com/my/course/login/web/LoginController.java
@@ -2,16 +2,19 @@ package com.my.course.login.web;
 
 import com.my.course.login.service.LoginService;
 import com.my.course.model.CommResult;
-import com.my.course.model.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lin.pingbin on 2018/1/4.
  */
 @RestController
+@CrossOrigin
 public class LoginController {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
     @Autowired
